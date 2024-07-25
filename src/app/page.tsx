9ec62072c9fb6167ee58a9a794c71d50ea614a17
Bikash Mishra <mishrabikash202@gmail.com>
2024-07-25 19:06:06 +0530

    clerk setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 
 export default function Home() {
   return (
    <main>
     Running...
+   <Button><Link href="/dashboard">Get Started</Link></Button>
    </main>
   );
 }
